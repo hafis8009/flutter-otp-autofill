6e28822fb8d02af9da24db60bf74e553f378c062
hafis8009 <hafis8009@gmail.com>
2025-09-07 13:52:16 +0300

    Fix applied for error occured in getAppSignatures

--- android/src/main/kotlin/ru/surfstudio/otp_autofill/AppSignatureHelper.kt
+++ android/src/main/kotlin/ru/surfstudio/otp_autofill/AppSignatureHelper.kt
@@ -16,38 +16,21 @@ private const val NUM_BASE64_CHAR = 11
 // https://github.com/googlearchive/android-credentials/blob/master/sms-verification/android/app/src/main/java/com/google/samples/smartlock/sms_verify/AppSignatureHelper.java
 class AppSignatureHelper(context: Context) : ContextWrapper(context) {
 
-    @Suppress("DEPRECATION")
-fun getAppSignatures(): List<String> {
-    return try {
-        val packageName = packageName
-        val packageManager = packageManager
-
-        val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            packageManager.getPackageInfo(
-                packageName,
-                PackageManager.GET_SIGNING_CERTIFICATES
+    fun getAppSignatures(): List {
+        return try {
+            val packageName = packageName
+            val packageManager = packageManager
+            val packageInfo = packageManager.getPackageInfo(
+            packageName,
+            PackageManager.GET_SIGNING_CERTIFICATES
             )
-        } else {
-            packageManager.getPackageInfo(
-                packageName,
-                PackageManager.GET_SIGNATURES
-            )
+            val signatures = packageInfo.signingInfo?.apkContentsSigners
+
+            signatures?.mapNotNull { hash(packageName, it.toCharsString()) } ?: emptyList()
+        } catch (e: PackageManager.NameNotFoundException) {
+            emptyList()
         }
-
-        val signatures = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            packageInfo.signingInfo?.apkContentsSigners
-        } else {
-            packageInfo.signatures
-        }
-
-        signatures?.mapNotNull { sig ->
-            hash(packageName, sig.toCharsString())
-        } ?: emptyList()
-    } catch (e: Exception) {
-        emptyList()
-    }
-}
-
+    }   
 
     private fun hash(packageName: String, signature: String): String? {
         val appInfo = "$packageName $signature"
